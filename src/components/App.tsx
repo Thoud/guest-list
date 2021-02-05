bd1d37ca8311cffed2b33d779e89c993b826acf0
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-02-05 15:07:08 +0100

    Add general styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import GuestList from './GuestList';
+import NewGuest from './NewGuest';
+import EventDetails from './EventDetails';
 
-const test = css`
-  width: 1000px;
-  height: 1000px;
-  background-color: red;
+const headingStyle = css`
+  font-size: 2.667rem;
+  margin: 4.115vw 0 2.135vw 4.792vw;
 `;
 
 export default function App() {
-  return <div css={test} />;
+  return (
+    <>
+      <h1 css={headingStyle}>Guest List Manager</h1>
+      <EventDetails />
+      <NewGuest />
+      <GuestList />
+    </>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,16 @@ import App from './components/App';
 const globalStyles = css`
   * {
     box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 18px;
+    color: #4d4545;
   }
 
-  html {
+  body {
     margin: 0;
     width: 100%;
+    background-color: #d9d9d9;
   }
 `;
 
